c81fdac534edd33e55162f48c79f1f3dd72a2114
vicentinileonardo <vicentini.leonardo99@gmail.com>
2025-02-17 17:55:14 +0100

    feat: first commit

---
+++ thesis_source/compile.sh
@@ -0,0 +1,10 @@
+setopt NULL_GLOB
+rm -f *.aux *.bbl *.blg *.lof *.log *.lot *.out *.toc *.fls *.fdb_latexmk
+unsetopt NULL_GLOB
+
+pdflatex -interaction=nonstopmode surname_name_mastercourse_ay.tex
+
+pdflatex -interaction=nonstopmode surname_name_mastercourse_ay.tex
+bibtex surname_name_mastercourse_ay
+pdflatex -interaction=nonstopmode surname_name_mastercourse_ay.tex
+pdflatex -interaction=nonstopmode surname_name_mastercourse_ay.tex
