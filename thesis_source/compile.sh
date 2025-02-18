8c6bd357ec29f1afbdd822eec8ad6899bfbd3c30
vicentinileonardo <vicentini.leonardo99@gmail.com>
2025-02-18 19:54:34 +0100

    fix: update

--- thesis_source/compile.sh
+++ thesis_source/compile.sh
@@ -2,7 +2,7 @@ setopt NULL_GLOB
 rm -f *.aux *.bbl *.blg *.lof *.log *.lot *.out *.toc *.fls *.fdb_latexmk
 unsetopt NULL_GLOB
 
-pdflatex -interaction=nonstopmode surname_name_mastercourse_ay.tex
-bibtex surname_name_mastercourse_ay
-pdflatex -interaction=nonstopmode surname_name_mastercourse_ay.tex
-pdflatex -interaction=nonstopmode surname_name_mastercourse_ay.tex
+pdflatex -interaction=nonstopmode vicentini_leonardo_cs_23-24.tex
+bibtex vicentini_leonardo_cs_23-24
+pdflatex -interaction=nonstopmode vicentini_leonardo_cs_23-24.tex
+pdflatex -interaction=nonstopmode vicentini_leonardo_cs_23-24.tex
